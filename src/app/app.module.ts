16fc72803d704ea8a0e77e23e7c62fefea4d7f46
Alexander Reyes <alex@Alexanders-MacBook-Pro.local>
2022-08-08 17:55:52 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OktaAuthModule, OKTA_CONFIG } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { ProfileComponent } from './profile/profile.component';
+
+const  yourOktaDomain = 'dev-41696345.okta.com';
+const  yourClientID = '0oa62opodsO5hopLI5d7';
+const oktaAuth = new OktaAuth({
+  issuer: `https://${yourOktaDomain}/oauth2/default`,
+  clientId: `${yourClientID}`,
+  redirectUri: window.location.origin + '/login/callback'
+});
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OktaAuthModule
   ],
-  providers: [],
+  providers: [{provide: OKTA_CONFIG, useValue: {oktaAuth}}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
